6327e9e4de9557c98d6f982887226a394cd79b63
whoamif <adjissifatimaamina@gmail.com>
2024-02-23 02:30:16 +0100

    up

--- env/app.py
+++ env/app.py
@@ -7,18 +7,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# CORS settings
-origins = [
-    "https://beautyplex0-qs8tenldq-beautyplexs-projects.vercel.app", 
-    "https://beautyplex0.vercel.app",  
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["*"],
-)
 
 class UserInfo(BaseModel):
     name: str
